c4709a5c74d15cb977e0fc47f8f908c1b6c8c561
puddly <32534428+puddly@users.noreply.github.com>
2020-06-13 16:58:13 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# zha-custom-radios
+
+Adds support for custom radio modules to Home Assistant's [ZHA (Zigbee Home Automation) integration component](https://www.home-assistant.io/integrations/zha/). This custom component allows users to test out new modules before they're integrated into ZHA and develop new zigpy radio modules without having to modify the Home Assistant source code.
+
+## Installation
+
+If you're using HACS, add `https://github.com/zha-ng/zha-custom-radios` as [a custom repository](https://hacs.xyz/docs/faq/custom_repositories). Otherwise, copy `custom_components/custom_zha_radios` into your Home Assistant installation's `custom_components` folder.
+
+The latest release of [`zigpy-znp`](https://github.com/zha-ng/zigpy-znp) is already a dependency in the `manifest.json` so all you need to do is add the following section to your `configuration.yaml`:
+
+```yaml
+zha_custom_radios:
+  znp:
+    module: zigpy_znp.zigbee.application
+    description: TI CC13x2, CC26x2, and ZZH
+```
+
+The latest release of `zigpy-znp` will be installed every time you restart Home Assistant.
---
+++ custom_components/zha_custom_radios/__init__.py
@@ -0,0 +1,49 @@
+import importlib
+
+from homeassistant.components.zha.core.const import RadioType
+
+
+DOMAIN = "zha_custom_radios"
+
+
+def inject_enum_member(target_enum, name, value):
+    """
+    Hack to inject a new member into an enum.
+    """
+
+    member = target_enum._member_type_.__new__(target_enum)
+    member._name_ = name
+    member._value_ = value
+
+    if not isinstance(value, tuple):
+        args = (value,)
+    else:
+        args = value
+
+    target_enum.__init__(member, *args)
+
+    target_enum._member_names_.append(name)
+    target_enum._member_map_[name] = member
+    target_enum._value2member_map_[value] = member
+    type.__setattr__(target_enum, name, member)
+
+
+def setup(hass, config):
+    """
+    Injects modules specified in `zha_custom_radios`'s configration into `zha`.
+    """
+
+    custom_names = list(config[DOMAIN].keys())
+    original_names = list(RadioType._member_names_)
+
+    for name, obj in config[DOMAIN].items():
+        module = importlib.import_module(obj["module"])
+        app = module.ControllerApplication
+        description = obj["description"]
+
+        inject_enum_member(RadioType, name, (description, app))
+
+    # New keys are moved up top
+    RadioType._member_names_ = custom_names + original_names
+
+    return True
